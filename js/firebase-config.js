6d51979a1bccfb3177eaf21848c7d642a60cbe4b
RemasIT0 <remaslove281@gmail.com>
2026-07-02 21:23:59 -0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDREIECI3uhbW6-6Zj8cMzFCL12JdNRAAs",
+  authDomain: "encuesta-dae5e.firebaseapp.com",
+  projectId: "encuesta-dae5e",
+  storageBucket: "encuesta-dae5e.firebasestorage.app",
+  messagingSenderId: "994636065828",
+  appId: "1:994636065828:web:93836e1f01fa0536bcb1",
+  measurementId: "G-FVL969JT35"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export { collection, addDoc, getDocs, signInWithEmailAndPassword, signOut, onAuthStateChanged };
